b016aeb7235b8ef197286e518ad67894d343c6be
Jacqueline Freeland <secretartist272@gmail.com>
2020-01-05 15:52:37 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,7 +25,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
   
 def valid_phone_number?(phone)
-  if text.match(/^[A-Z]/) && text.match(/[\.]$/)
+  if if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
     true
   else 
     false
